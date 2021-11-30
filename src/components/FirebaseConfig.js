9c48baf65d0efa3a1ea2c034cebbb81a74a07997
Amanda Lin <amanda.lin.aut@gmail.com>
2021-11-30 08:42:42 -0500

    Fixed warnings

--- src/components/FirebaseConfig.js
+++ src/components/FirebaseConfig.js
@@ -1,5 +1,5 @@
 // Your web app's Firebase configuration
-export default {
+const FirebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "exercise-five-e60c8.firebaseapp.com",
   projectId: "exercise-five-e60c8",
@@ -7,3 +7,5 @@ export default {
   messagingSenderId: process.env.REACT_APP_MESSAGING_ID,
   appId: process.env.REACT_APP_APP_ID,
 };
+
+export default FirebaseConfig;
